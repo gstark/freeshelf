4878313fc2602276674753921d5d8bbe373668cf
Gavin Stark <gavin@gstark.com>
2017-08-29 13:58:52 -0400

    Step 2: User creation via controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
-Rails.application.routes.draw do
+jails.application.routes.draw do
   resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
