22ec7b431b0feaab16fda6ef598dcd765f70fb16
Gavin Stark <gavin@gstark.com>
2017-08-29 14:03:56 -0400

    Step3: Adds login and filter to prevent visiting books controller without being logged in

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authorize!
 
   # Like in express...  
   # app.get('/books', (req, res) => {
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
-jails.application.routes.draw do
+Rails.application.routes.draw do
   resources :books
+  root 'books#index'
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
 end
